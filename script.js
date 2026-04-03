f1a0009790d1ff9100c7e5af5faf6c359f8584eb
zakabdouli-ux <zakabdouli@gmail.com>
2026-04-03 15:01:45 +0200

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const map = L.map('map').setView([31,5],5);
+const map = L.map('map').setView([31, 5], 5);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; OpenStreetMap & Carto'
 }).addTo(map);
