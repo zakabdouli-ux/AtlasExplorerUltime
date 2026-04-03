42205d0927ad5e49a4f16151c19146a11cc87b2a
zakabdouli-ux <zakabdouli@gmail.com>
2026-04-03 16:17:31 +0200

    script.js

--- script.js
+++ script.js
@@ -3,50 +3,36 @@ const map = L.map('map').setView([30,5],5);
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(map);
 
 // ================= DATA =================
-
 const countries = {
 "Morocco":{
 center:[31.5,-7],
 zoom:6,
 name:"Maroc 🇲🇦",
 image:"https://images.unsplash.com/photo-1548013146-72479768bada?w=1200",
-desc:`Destination incontournable entre désert, montagnes et villes impériales.
-Idéal pour culture, gastronomie et aventure.`,
-type:"culture"
+desc:"Destination riche entre désert, montagnes et villes impériales."
 },
-
 "Algeria":{
 center:[28,2],
 zoom:5,
 name:"Algérie 🇩🇿",
 image:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1200",
-desc:`Pays immense avec Sahara, montagnes et Méditerranée.
-Destination authentique encore peu touristique.`,
-type:"nature"
+desc:"Pays immense entre mer et Sahara."
 },
-
 "Tunisia":{
 center:[34,10],
 zoom:6,
 name:"Tunisie 🇹🇳",
 image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=1200",
-desc:`Destination idéale pour plages et culture.
-Parfait pour vacances accessibles.`,
-type:"plage"
+desc:"Plages, culture et soleil."
 }
 };
 
-// ================= FRONTIÈRES RÉELLES =================
-
-fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
+// ================= FRONTIÈRES LOCALES =================
+fetch('countries.geojson')
 .then(res=>res.json())
 .then(data=>{
 
-let selected = data.features.filter(f =>
-["Morocco","Algeria","Tunisia"].includes(f.properties.ADMIN)
-);
-
-L.geoJSON(selected,{
+L.geoJSON(data,{
 style:{
 color:"#22c55e",
 fillColor:"#22c55e",
@@ -67,7 +53,6 @@ showCountry(feature.properties.ADMIN);
 });
 
 // ================= AFFICHAGE =================
-
 function showCountry(name){
 const c = countries[name];
 
@@ -79,33 +64,3 @@ document.getElementById("info").innerHTML = `
 <div class="card">${c.desc}</div>
 `;
 }
-
-// ================= VILLES =================
-
-const cities = [
-["Marrakech",31.63,-8,"Souks, palais et ambiance unique","Morocco"],
-["Fès",34.02,-5,"Ville historique UNESCO","Morocco"],
-["Casablanca",33.57,-7.58,"Ville moderne","Morocco"],
-
-["Alger",36.75,3.06,"Capitale méditerranéenne","Algeria"],
-["Oran",35.7,-0.6,"Ville du raï","Algeria"],
-["Constantine",36.36,6.61,"Ville des ponts","Algeria"],
-
-["Tunis",36.8,10.18,"Capitale historique","Tunisia"],
-["Sousse",35.83,10.64,"Station balnéaire","Tunisia"],
-["Djerba",33.8,10.8,"Île paradisiaque","Tunisia"]
-];
-
-cities.forEach(c=>{
-let marker = L.marker([c[1],c[2]]).addTo(map);
-
-marker.on("click",()=>{
-map.flyTo([c[1],c[2]],8);
-
-document.getElementById("info").innerHTML = `
-<h2>${c[0]}</h2>
-<div class="card">${c[3]}</div>
-<p><strong>Pays :</strong> ${c[4]}</p>
-`;
-});
-});
