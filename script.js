078be1ea492527f84d7636de038766952046f42c
zakabdouli-ux <zakabdouli@gmail.com>
2026-04-03 16:47:38 +0200

    script.js

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const map = L.map('map').setView([31,5],5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: "Map"
 }).addTo(map);
+// ================= FRONTIÈRES =================
+
+// Maroc
+const maroc = L.polygon([
+[27,-13],[32,-9],[35,-6],[35,-2],[30,-1],[28,-7]
+]).addTo(map);
+
+// Algérie
+const algerie = L.polygon([
+[19,-8],[28,-2],[35,3],[36,8],[30,11],[22,4]
+]).addTo(map);
+
+// Tunisie
+const tunisie = L.polygon([
+[30,7],[33,9],[37,10],[34,11],[31,10]
+]).addTo(map);
