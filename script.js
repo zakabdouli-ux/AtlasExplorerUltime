386aff06109a03322ef90d4784a52e18957cb222
zakabdouli-ux <zakabdouli@gmail.com>
2026-04-03 16:37:51 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-// création carte
+// ================= CARTE =================
 const map = L.map('map').setView([31,5],5);
 
-// fond de carte
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// ================= DONNÉES =================
+const countries = {
+"Maroc":{
+name:"Maroc 🇲🇦",
+image:"https://images.unsplash.com/photo-1548013146-72479768bada?w=1200",
+desc:"Le Maroc est une destination exceptionnelle entre désert, montagnes et villes impériales."
+},
+"Algérie":{
+name:"Algérie 🇩🇿",
+image:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1200",
+desc:"L’Algérie offre des paysages incroyables entre mer et Sahara."
+},
+"Tunisie":{
+name:"Tunisie 🇹🇳",
+image:"https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=1200",
+desc:"La Tunisie combine plages et patrimoine."
+}
+};
+
+// ================= AFFICHAGE =================
+function showCountry(name){
+const c = countries[name];
+
+document.getElementById("info").innerHTML = `
+<h2>${c.name}</h2>
+<img src="${c.image}">
+<div class="card">${c.desc}</div>
+`;
+}
 
 // ================= FRONTIÈRES =================
 
@@ -23,6 +52,6 @@ const tunisie = L.polygon([
 
 // ================= INTERACTIONS =================
 
-maroc.on("click", ()=> alert("Maroc 🇲🇦"));
-algerie.on("click", ()=> alert("Algérie 🇩🇿"));
-tunisie.on("click", ()=> alert("Tunisie 🇹🇳"));
+maroc.on("click", ()=> showCountry("Maroc"));
+algerie.on("click", ()=> showCountry("Algérie"));
+tunisie.on("click", ()=> showCountry("Tunisie"));
